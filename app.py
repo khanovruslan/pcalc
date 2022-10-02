ab7711448490d43a1fc05c66d8be89063e0c2eee
gregoryit <mike25012000@gmail.com>
2022-10-02 23:29:15 +0300

    add power by

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print("Hi, what you gonna do today?")
-print('1 - sum\n2 - subtraction\n3 - multiplication\n4 - division')
+print('1 - sum\n2 - subtraction\n3 - multiplication\n4 - division\n5 - power by')
 q = int(input('your choice '))
 if q == 1:
     a = int(input('first number please '))
@@ -20,6 +20,11 @@ elif q == 4:
     a = int(input('first number please '))
     b = int(input('second number please '))
     print('result', a / b)
+
+elif q == 5:
+    a = int(input('first number please '))
+    b = int(input('second number please '))
+    print('result', a ** b)
     
 else:
     print('wrong operation')
