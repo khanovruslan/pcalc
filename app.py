499788b8a0f1a0f7a9e0e625976e8df58c6771a0
khanovruslan <khanovra@gmail.com>
2022-10-02 14:45:08 -0400

    added muliplication by num 3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print("Hi, what you gonna do today?")
-print('1 - sum\n2 - subtraction')
+print('1 - sum\n2 - subtraction\n3 - multiplication')
 q = int(input('your choice '))
 if q == 1:
     a = int(input('first number please '))
@@ -10,6 +10,11 @@ elif q == 2:
     a = int(input('first number please '))
     b = int(input('second number please '))
     print('result', a - b)
+
+elif q == 3:
+    a = int(input('first number please '))
+    b = int(input('second number please '))
+    print('result', a * b)
 
 else:
     print('wrong operation')
