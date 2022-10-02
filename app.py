3d63f0b9289836f8e10429091c37b4fc599d40ee
khanovruslan <khanovra@gmail.com>
2022-10-02 14:42:27 -0400

    added subtraction by  num 2

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 print("Hi, what you gonna do today?")
-print('1 - sum')
+print('1 - sum\n2 - subtraction')
 q = int(input('your choice '))
 if q == 1:
     a = int(input('first number please '))
     b = int(input('second number please '))
-    print(a + b)
+    print('result', a + b)
+
+elif q == 2:
+    a = int(input('first number please '))
+    b = int(input('second number please '))
+    print('result', a - b)
+
 else:
     print('wrong operation')
